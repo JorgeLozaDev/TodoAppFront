f5be7de98279c99528995d40067d81b65a2bf01e
jorge <j0rg3e93@gmail.com>
2024-01-16 23:04:34 +0100

    ADD & UPDATE: Se crea la pagina de registro, en body se agrega su url,

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../Home/Home";
+import SignIn from "../SignIn/SignIn";
 
 const Body = () => {
   return (
     <Routes>
       <Route path="*" element={<Navigate to="/" />} />
       <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<SignIn />} />
     </Routes>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,37 @@
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    Grid,
+    Typography
+} from "@mui/material";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Grid
+        container
+        spacing={2}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: "100vh" }}
+      >
+        <Grid item xs></Grid>
+        <Grid
+          item
+          xs={8}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          
+        </Grid>
+        <Grid item xs></Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default Home;
