d0c12c959d4c1c434206d2230f106b79a100f5ac
jorge <j0rg3e93@gmail.com>
2024-01-17 22:47:26 +0100

    ADD & UPDATE: Se crea el componente para el registro de usuarios y se agrega en el body su enlace

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../Home/Home";
 import SignIn from "../SignIn/SignIn";
+import Register from "../Register/Register";
 
 const Body = () => {
   return (
@@ -8,6 +9,7 @@ const Body = () => {
       <Route path="*" element={<Navigate to="/" />} />
       <Route path="/" element={<Home />} />
       <Route path="/signin" element={<SignIn />} />
+      <Route path="/signin" element={<Register />} />
     </Routes>
   );
 };
