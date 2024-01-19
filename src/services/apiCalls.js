5afc7004209b8a9e81e64290a8af2e7152212da2
jorge <j0rg3e93@gmail.com>
2024-01-19 22:42:27 +0100

    ADD: Se agrega ek archivo de apicall, para manejar las consultas a la base de datos

---
+++ src/services/apiCalls.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const URL = "http://localhost:3000/";
+
+
+export const singInUser = async (endpoint, data) => {
+  const inUser = await axios.post(`${URL}${endpoint}`, data);
+  return inUser;
+};
