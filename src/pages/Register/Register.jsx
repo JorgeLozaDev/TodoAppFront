7be4594d55e8ae0de338f801c07f5fb6d2b0a0fe
jorge <j0rg3e93@gmail.com>
2024-01-19 22:28:35 +0100

    UPDATE: Se actualiza el codigo y se simplifica, se utiliza un nuevo componente donde cargaremos el form

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,99 @@
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import RegisterForm from "./Components/RegisterForm";
 
+const defaultTheme = createTheme();
 
-const Register = () => {
+export default function SignUp() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({
+    firstName: "",
+    email: "",
+    password: "",
+  });
+
+  const isEmailValid = (email) => {
+    // Expresión regular simple para verificar el formato de un correo electrónico
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Validaciones al hacer submit
+    const newErrors = {
+      firstName: formData.firstName ? "" : "Este campo es obligatorio",
+      email: isEmailValid(formData.email)
+        ? ""
+        : "Por favor, introduce un email válido",
+      password:
+        formData.password.length >= 6
+          ? ""
+          : "La contraseña debe tener al menos 6 caracteres",
+    };
+
+    setErrors(newErrors);
+
+    // Comprobar si hay errores antes de continuar
+    if (Object.values(newErrors).every((error) => error === "")) {
+      // Realizar acciones de envío o validación final aquí
+      console.log(formData);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
-    <div>Register</div>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <RegisterForm
+              formData={formData}
+              handleSubmit={handleSubmit}
+              handleInputChange={handleInputChange}
+              errors={errors}
+            />
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
-
-export default Register
