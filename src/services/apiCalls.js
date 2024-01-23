ee2ca7bb6f5c41fc6fa8f05a221b946564cd8b66
jorge <j0rg3e93@gmail.com>
2024-01-23 22:10:03 +0100

    UPDATE: Se agrega una funcion para hacer las llamadas a la base de datos

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -1,9 +1,35 @@
 import axios from "axios";
 
-const URL = "http://localhost:3000/";
+const baseURL = "http://localhost:3000/";
 
+export const general = async (verbo, endpoint, header, data) => {
+  // Validaciones para asegurar que se proporcionen el verbo y el endpoint
+  if (!verbo || !endpoint) {
+    console.error("Se deben proporcionar el verbo y el endpoint.");
+    throw new Error("Falta el verbo o el endpoint en la llamada a la API.");
+  }
 
-export const singInUser = async (endpoint, data) => {
-  const inUser = await axios.post(`${URL}${endpoint}`, data);
-  return inUser;
+  try {
+    const axiosConfig = {
+      method: verbo,
+      url: `${baseURL}${endpoint}`,
+    };
+
+    // Agregar headers si se proporcionan
+    if (header) {
+      axiosConfig.headers = { Authorization: `Bearer ${header}` };
+    }
+
+    // Agregar datos si se proporcionan
+    if (data) {
+      axiosConfig.data = data;
+    }
+
+    const response = await axios(axiosConfig);
+    return response.data;
+  } catch (error) {
+    // Manejo de errores (puedes personalizar según tus necesidades)
+    console.error("Error en la llamada a la API:", error);
+    throw error;
+  }
 };
