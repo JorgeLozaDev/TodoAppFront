a0c7c3cff8bca836c82da87c31f640a2c0934fbf
Jorge <j0rg3e93@gmail.com>
2024-03-21 11:26:35 +0100

    UPDATE: Se agregan los import necesarios y envolvemos el app con el store para manejar los estados

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,21 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
+import { Provider } from "react-redux";
+import store from "./app/store.js";
+import persistStore from "redux-persist/es/persistStore";
+import { PersistGate } from "redux-persist/integration/react";
+const persistore = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <CssBaseline />
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistore}>
+        <BrowserRouter>
+          <CssBaseline />
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode> 
 );
