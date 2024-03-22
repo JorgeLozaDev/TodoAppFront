1cae93a2ce4d93839b0a2bbd441dbac59c06de9b
Jorge <j0rg3e93@gmail.com>
2024-03-22 11:40:04 +0100

    UPDATE: Se agrega el codigo necesario para saber si el usuario esta logueado o no, si no lo esta se le envia a la home

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
+import { userDetails } from '../userSlice';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const Profile = () => {
+  const token = useSelector(userDetails);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token.credentials == "") {
+      navigate("/");
+    }
+  }, []);
   return (
-    <div>Profile</div>
+    <div>Profile {token.credentials}</div>
   )
 }
