d8d9e1908226c6efd8904a064a0669f719470ba8
jorge <j0rg3e93@gmail.com>
2024-07-25 00:34:27 +0200

    UPDATE: Se ajusta la función del logout y como inicializamos los credentials

--- src/pages/userSlice.js
+++ src/pages/userSlice.js
@@ -1,26 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const userSlice = createSlice({
-    name: 'user',
-    initialState: {
-      credentials: ''
+  name: "user",
+  initialState: {
+    credentials: null,
+  },
+  reducers: {
+    login: (state, action) => {
+      return {
+        ...state,
+        ...action.payload,
+      };
     },
-    reducers: {
-      login: (state, action) => {
-        return {
-          ...state,
-          ...action.payload
-        }
-      },
-      logout: (state, action) => {
-        return {
-          ...state,
-          ...action.payload
-        }
-      }
-      
-    }
-    
+    logout: (state) => {
+      return {
+        ...state,
+        credentials: null,
+      };
+    },
+  },
 });
 
 export const { login, logout } = userSlice.actions;
