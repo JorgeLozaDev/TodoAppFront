b32a8e8cbce8067d12a5ae5063dbdd8d9bd681fd
jorge <j0rg3e93@gmail.com>
2024-07-29 23:29:34 +0200

    UPDATE: Se hacen modificaciones para sacar la información de la API

--- src/pages/Profile/components/ProfileData/ProfileData.jsx
+++ src/pages/Profile/components/ProfileData/ProfileData.jsx
@@ -1,17 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Typography, Button } from "@mui/material";
 import { ProfileForm } from "../ProfileForm/ProfileForm";
+import { general } from "../../../../services/apiCalls";
+import { jwtDecode } from "jwt-decode";
+import { handleAxiosError } from "../../../../utils/axiosErrorHandler";
 
 export const ProfileData = ({ userData, handleSave }) => {
   const [editMode, setEditMode] = useState(false);
 
   // Datos de usuario por defecto
-  userData = {
+  const userData1 = {
     name: "John Doe",
     email: "john.doe@example.com",
   };
 
-  const [formData, setFormData] = useState(userData);
+  const [formData, setFormData] = useState(userData1);
 
   const handleEditClick = () => {
     setEditMode(true);
@@ -26,9 +29,23 @@ export const ProfileData = ({ userData, handleSave }) => {
     handleSave(updatedData);
     setEditMode(false);
   };
+  
+  // const { id } = userData;
+
+  useEffect(() => {
+    
+
+    general("get", "user/profile", userData, null)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        handleAxiosError(error);
+      });
+  }, []);
 
   return (
-    <Box >
+    <Box>
       {editMode ? (
         <ProfileForm
           formData={formData}
@@ -40,11 +57,11 @@ export const ProfileData = ({ userData, handleSave }) => {
         <Box>
           <Box sx={{ mb: 2 }}>
             <Typography variant="h6">Nombre:</Typography>
-            <Typography variant="body2">{userData.name}</Typography>
+            <Typography variant="body2">{userData1.name}</Typography>
           </Box>
           <Box sx={{ mb: 2 }}>
             <Typography variant="h6">Email:</Typography>
-            <Typography variant="body2">{userData.email}</Typography>
+            <Typography variant="body2">{userData1.email}</Typography>
           </Box>
           <Button variant="contained" color="primary" onClick={handleEditClick}>
             Modificar
