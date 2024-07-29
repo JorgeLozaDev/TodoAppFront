fea0deae80a2fbfba8e8b1c436451b91cafe19a9
jorge <j0rg3e93@gmail.com>
2024-07-29 23:28:44 +0200

    ADD: se añade una funcion para ayudar a validar la caducidad de los tokens

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,12 @@
+import { jwtDecode } from "jwt-decode";
+
 export const isEmailValid = (email) => {
   // Expresión regular simple para verificar el formato de un correo electrónico
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
+};
+
+export const isTokenExpired = (token) => {
+  const decode = jwtDecode(token);
+  return Date.now() > decode.exp * 1000;
 };
