2d5b6f86e5921c71b01770b3962a30a6b4c35033
jorge <j0rg3e93@gmail.com>
2024-07-30 09:36:42 +0200

    UDPATE: Se ajusta para que en este archivo controle todo lo necesario para el form

--- src/pages/Profile/components/ProfileData/ProfileData.jsx
+++ src/pages/Profile/components/ProfileData/ProfileData.jsx
@@ -1,20 +1,29 @@
+import { Box, Button, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { Box, Typography, Button } from "@mui/material";
-import { ProfileForm } from "../ProfileForm/ProfileForm";
 import { general } from "../../../../services/apiCalls";
-import { jwtDecode } from "jwt-decode";
 import { handleAxiosError } from "../../../../utils/axiosErrorHandler";
+import { ProfileForm } from "../ProfileForm/ProfileForm";
 
 export const ProfileData = ({ userData, handleSave }) => {
   const [editMode, setEditMode] = useState(false);
+  const [errors, setErrors] = useState({
+    nombre: "",
+    email: "",
+    password: "",
+  });
 
   // Datos de usuario por defecto
-  const userData1 = {
-    name: "John Doe",
-    email: "john.doe@example.com",
-  };
 
-  const [formData, setFormData] = useState(userData1);
+  const [userDataInicial, setuserDataInicial] = useState({
+    nombre: "",
+    email: "",
+  });
+
+  const [formData, setFormData] = useState({
+    nombre: "",
+    email: "",
+    password: null,
+  });
 
   const handleEditClick = () => {
     setEditMode(true);
@@ -22,46 +31,77 @@ export const ProfileData = ({ userData, handleSave }) => {
 
   const handleCancelClick = () => {
     setEditMode(false);
-    setFormData(userData); // Reset form data on cancel
+    setFormData(userDataInicial); // Reset form data on cancel
   };
 
-  const handleSaveClick = (updatedData) => {
-    handleSave(updatedData);
+  const handleSubmit = (event) => {
+    // handleSave(updatedData);
     setEditMode(false);
   };
-  
+
   // const { id } = userData;
 
   useEffect(() => {
-    
-
     general("get", "user/profile", userData, null)
       .then((data) => {
-        console.log(data);
+        setFormData(data.data);
+        setuserDataInicial(data.data);
       })
       .catch((error) => {
         handleAxiosError(error);
       });
   }, []);
 
+  const validate = (fieldValues = formData) => {
+    let tempErrors = { ...errors };
+
+    if ("name" in fieldValues)
+      tempErrors.name = fieldValues.name
+        ? fieldValues.name.length >= 3
+          ? ""
+          : "El nombre debe tener al menos 3 caracteres."
+        : "El nombre es requerido.";
+    if ("email" in fieldValues)
+      tempErrors.email = /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(fieldValues.email)
+        ? ""
+        : "Email no válido.";
+
+    if ("password" in fieldValues)
+      tempErrors.password.length >= 6
+        ? ""
+        : "La contraseña debe tener al menos 6 caracteres";
+    setErrors(tempErrors);
+    return Object.values(tempErrors).every((x) => x === "");
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    validate({ [name]: value });
+  };
+
   return (
     <Box>
       {editMode ? (
         <ProfileForm
           formData={formData}
-          setFormData={setFormData}
-          handleSave={handleSaveClick}
-          handleCancel={handleCancelClick}
+          handleSubmit={handleSubmit}
+          handleInputChange={handleInputChange}
+          handleCancelClick={handleCancelClick}
+          errors={errors}
         />
       ) : (
         <Box>
           <Box sx={{ mb: 2 }}>
             <Typography variant="h6">Nombre:</Typography>
-            <Typography variant="body2">{userData1.name}</Typography>
+            <Typography variant="body2">{userDataInicial.nombre}</Typography>
           </Box>
           <Box sx={{ mb: 2 }}>
             <Typography variant="h6">Email:</Typography>
-            <Typography variant="body2">{userData1.email}</Typography>
+            <Typography variant="body2">{userDataInicial.email}</Typography>
           </Box>
           <Button variant="contained" color="primary" onClick={handleEditClick}>
             Modificar
