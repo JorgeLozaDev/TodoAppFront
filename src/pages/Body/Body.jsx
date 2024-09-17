afc47ade04003d1690aa4f0b782b164495e6c2e1
jorge <j0rg3e93@gmail.com>
2024-09-17 23:57:14 +0200

    UPDATE: Se agregan nuevas rutas para los todos

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -3,6 +3,8 @@ import Home from "../Home/Home";
 import SignIn from "../SignIn/SignIn";
 import Register from "../Register/Register";
 import { Profile } from "../Profile/Profile";
+import { TodoForm } from "../Profile/components/Todo/TodoForm/TodoForm";
+import { TodoList } from "../Profile/components/Todo/TodoList/TodoList";
 
 const Body = () => {
   return (
@@ -12,6 +14,8 @@ const Body = () => {
       <Route path="/signin" element={<SignIn />} />
       <Route path="/register" element={<Register />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/profile/todos/list" element={<TodoList />} />
+      <Route path="/profile/todos/form" element={<TodoForm />} />
     </Routes>
   );
 };
