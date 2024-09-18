e527bad4ada42a0224661fc70ada2e9e66f8d936
jorge <j0rg3e93@gmail.com>
2024-09-18 23:54:53 +0200

    UPDATE: Se agregan librerias de textarea y datapicker, se da un formato inicial del formulario

--- src/pages/Profile/components/Todo/TodoForm/TodoForm.jsx
+++ src/pages/Profile/components/Todo/TodoForm/TodoForm.jsx
@@ -1,5 +1,5 @@
 import { Button, TextField, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import { Box, Container } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import { isEmailValid, isTokenExpired } from "../../../../../utils/helpers";
 import { useDispatch, useSelector } from "react-redux";
@@ -7,6 +7,9 @@ import { useNavigate } from "react-router-dom";
 import { logout, userDetails } from "../../../../userSlice";
 import { Toasty } from "../../../../../common/CustomToasty/CustomToasty";
 import { ToastContainer } from "react-toastify";
+import { Textarea } from "@mui/joy";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 export const TodoForm = () => {
   const dispatch = useDispatch();
@@ -52,21 +55,64 @@ export const TodoForm = () => {
   return (
     <>
       <ToastContainer />
-      <Box component="form" onSubmit={onSubmit} sx={{ mt: 3 }}>
-        <Box sx={{ mb: 2 }}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            disabled
-            // fullWidth
-            id="email"
-            label="Correo Electrónico"
-            name="email"
-            onChange={handleInputChange}
-            error={Boolean(errors.email)}
-            FormHelperTextProps={{ style: { margin: 0, height: "20px" } }}
-          />
+      <Container component="main" maxWidth="md">
+        <Box component="form" onSubmit={onSubmit} sx={{ mt: 3 }}>
+          <Box sx={{ mb: 2 }}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              disabled
+              fullWidth
+              id="tituloTarea"
+              label="titulo tarea"
+              name="tituloTarea"
+              onChange={handleInputChange}
+              // error={Boolean(errors.email)}
+              FormHelperTextProps={{ style: { margin: 0, height: "20px" } }}
+            />
+          </Box>
+          <Box sx={{ mb: 2 }}>
+            <Textarea
+              color="neutral"
+              minRows={3}
+              placeholder="Descripción de la tarea"
+              size="lg"
+              variant="outlined"
+            />
+          </Box>
+          <Box sx={{ mb: 2 }}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker />
+            </LocalizationProvider>
+          </Box>
+          <Box sx={{ display: "flex", gap: 2, mb: 2 }}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              disabled
+              fullWidth
+              id="email"
+              label="Correo Electrónico"
+              name="email"
+              onChange={handleInputChange}
+              error={Boolean(errors.email)}
+              FormHelperTextProps={{ style: { margin: 0, height: "20px" } }}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="name"
+              label="Nombre"
+              name="nombre"
+              onChange={handleInputChange}
+              error={Boolean(errors.name)}
+              FormHelperTextProps={{ style: { margin: 0, height: "20px" } }}
+            />
+          </Box>
           <Typography
             variant="caption"
             color="error"
@@ -74,20 +120,6 @@ export const TodoForm = () => {
           >
             {errors.email}
           </Typography>
-        </Box>
-        <Box sx={{ mb: 2, width: 600, maxWidth: "100%" }}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            // fullWidth
-            id="name"
-            label="Nombre"
-            name="nombre"
-            onChange={handleInputChange}
-            error={Boolean(errors.name)}
-            FormHelperTextProps={{ style: { margin: 0, height: "20px" } }}
-          />
           <Typography
             variant="caption"
             color="error"
@@ -95,42 +127,42 @@ export const TodoForm = () => {
           >
             {errors.name}
           </Typography>
+
+          <Box sx={{ mb: 2 }}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              id="password"
+              label="Password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              onChange={handleInputChange}
+              error={Boolean(errors.password)}
+              FormHelperTextProps={{ style: { margin: 0, height: "20px" } }}
+            />
+            <Typography
+              variant="caption"
+              color="error"
+              sx={{ display: "block", height: "20px" }}
+            >
+              {errors.password}
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}>
+            <Button type="submit" variant="contained" color="primary">
+              Guardar
+            </Button>
+            <Button
+              variant="outlined"
+              color="secondary"
+              onClick={handleCancelClick}
+            >
+              Cancelar
+            </Button>
+          </Box>
         </Box>
-        <Box sx={{ mb: 2 }}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            // fullWidth
-            id="password"
-            label="Password"
-            name="password"
-            type="password"
-            autoComplete="current-password"
-            onChange={handleInputChange}
-            error={Boolean(errors.password)}
-            FormHelperTextProps={{ style: { margin: 0, height: "20px" } }}
-          />
-          <Typography
-            variant="caption"
-            color="error"
-            sx={{ display: "block", height: "20px" }}
-          >
-            {errors.password}
-          </Typography>
-        </Box>
-        <Box sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}>
-          <Button type="submit" variant="contained" color="primary">
-            Guardar
-          </Button>
-          <Button
-            variant="outlined"
-            color="secondary"
-            onClick={handleCancelClick}
-          >
-            Cancelar
-          </Button>
-        </Box>
-      </Box>
+      </Container>
     </>
   );
 };
